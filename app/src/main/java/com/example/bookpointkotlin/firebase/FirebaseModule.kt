9bf41fd1423d8e472bd1cf2fafc8d7eb662d680c
Sean Ratemo <sean.ratemo@gmail.com>
2024-11-26 11:52:33 +0300

    Drawables

---
+++ app/src/main/java/com/example/bookpointkotlin/firebase/FirebaseModule.kt
@@ -0,0 +1,28 @@
+package com.example.tutoria.firebase
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideFirebaseStorage(): StorageReference =
+        FirebaseStorage.getInstance().reference
+}
